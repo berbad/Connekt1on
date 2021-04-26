1fc406dd7502f4a12afbc36307177d32228466ca
mteichen <63485559+mteichen@users.noreply.github.com>
2021-04-26 12:53:32 -0500

    added login page template

--- src/main/java/Connekt1onApp/WelcomeController.java
+++ src/main/java/Connekt1onApp/WelcomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
-  @GetMapping("/welcome")
+  @GetMapping("/login")
   public String welsome(){
-    return "Hi Krstin";
+    return "Welcome, Connekt1on Placeholder";
   }
 
 }
