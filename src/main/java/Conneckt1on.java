065d4fe23b1cf13eaf52e1da2154027f70dfaff2
mteichen <63485559+mteichen@users.noreply.github.com>
2021-04-26 12:45:05 -0500

    Template for Heroku deployment

--- src/main/java/Conneckt1on.java
+++
@@ -1,3 +0,0 @@
-public class Conneckt1on {
-
-}
---
+++ src/main/java/Connekt1onApp/Connekt1onAppRunner.java
@@ -0,0 +1,13 @@
+package Connekt1onApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Connekt1onAppRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Connekt1onAppRunner.class, args);
+  }
+
+}
